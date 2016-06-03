83e80e31cb68f239eac4bfe75bfecf49758502f2
Flimmykins <flameo@hotmail.co.uk>
2016-06-03 21:00:43 +0100

    homepage

---
+++ random.js
@@ -0,0 +1,12 @@
+var random_images_array = ["1.png",
+"2.png",
+"3.png",
+"4.png",
+];
+
+path = 'corner/';
+var num = Math.floor( Math.random() * random_images_array.length );
+var img = random_images_array[ num ];
+var imgStr = '<img src="' + path + img + '">';
+
+document.write(imgStr); document.close();
