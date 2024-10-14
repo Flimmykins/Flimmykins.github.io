310b4794a5c861b53c3d1e563be48a0520190893
Flimmykins <flameo@hotmail.co.uk>
2024-10-14 19:02:57 +0100

    update

--- startpage/random.js
+++ startpage/random.js
@@ -1,15 +1,15 @@
 var random_images_array = ["1.png",
-"2.png",
-"3.png",
-"4.png",
-"5.png",
-"6.png",
-"7.png",
+    "2.png",
+    "3.png",
+    "4.png",
+    "5.png",
+    "6.png",
+    "7.png",
 ];
 
-path ='corner/'; 
-var num = Math.floor( Math.random() * random_images_array.length );
-var img = random_images_array[ num ];
+path = 'corner/';
+var num = Math.floor(Math.random() * random_images_array.length);
+var img = random_images_array[num];
 var imgStr = '<img src="' + path + img + '">';
 
 document.write(imgStr); document.close();
